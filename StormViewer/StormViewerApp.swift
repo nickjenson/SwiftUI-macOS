921a32497ef0f7233fa383a3bf929b6010340e54
Nick Jenson <nickjenson@gmail.com>
2022-04-19 17:07:31 -0700

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Nick Jenson on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
